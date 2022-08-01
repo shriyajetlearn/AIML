9d9885e0f830ad9757ffba3561cd70924e45b0ea
shriyajetlearn <104649309+shriyajetlearn@users.noreply.github.com>
2022-08-01 23:02:00 +0530

    Update AIML11.py

---
+++ AIML11.py
@@ -0,0 +1,64 @@
+# Rough notes 
+
+# NLP  - Natural Language Processing
+"""
+What is the langauge of computers ? Binary
+Human Language - English
+
+Human Language -> NLP -> Binary
+
+Supervised Learning
+
+Applications - 
+Smart Assistant
+Chatbots
+Plagarism Detection
+
+I am Aditya. - info
+I live in Qatar. - info
+I am 14 years old. - info
+
+Total Words - 10
+
+Stop-words - words which do not contribute to the event / helping verbs, pronouns, conjunctions
+
+Ex- I, am, in, and ......
+
+There is a cricket match at Dubai Stadium on 20-Oct. - event
+Where - Dubai Stadium
+What - Cricket Match
+When - 20-oct
+
+Aditya lives in Qatar.
+
+Aditya
+live Qatar
+years old
+
+Dictionary - 5
+
+  Aditya, live, Qatar, years, old  # Jumbled before
+1) 1       0      0     0      0
+2) 0       1      1     0      0
+3) 0       0      0     1      1
+4) 1       1      1     0      0
+
+
+Similarities between the sentences is measured by the overlap of values in the vector form when represented in the word dictionary.
+
+
+Sentiment Analysis (Supervised Machine Learning Problem)
+Classification  Problem
+
+Find the context / emotion begind the sentences
+
+I am not happy with the delivery services provided by amazon. --> Negative
+
+I am really satisfied with the product. Best Buy ! --> Positive
+
+
+Amazon Recommendations
+Facebook's Emojis
+Upvotes / Downvotes
+Brand Monitoring
+"""
